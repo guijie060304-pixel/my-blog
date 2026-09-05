f5814db8e229797d2d2dbc2ccb739a5677ea2dc8
guijie060304-pixel <guijie060304@gmail.com>
2026-09-06 00:30:20 +0800

    fix: open new post in VS Code with notepad fallback

--- 新建文章.bat
+++ 新建文章.bat
@@ -3,7 +3,14 @@ cd /d D:\Project\my-blog
 set /p T=请输入文章标题:
 if "%T%"=="" goto end
 hexo new "%T%"
-start "" notepad "source\_posts\%T%.md"
+set "LATEST="
+for /f "delims=" %%i in ('dir /b /o-d source\_posts\*.md') do if not defined LATEST set "LATEST=%%i"
+set "VSC=%LocalAppData%\Programs\Microsoft VS Code\Code.exe"
+if exist "%VSC%" (
+  start "" "%VSC%" "source\_posts\%LATEST%"
+) else (
+  start "" "%SystemRoot%\notepad.exe" "source\_posts\%LATEST%"
+)
 echo.
 echo 文章已创建并打开，写完后双击「发布上线.bat」即可
 :end
